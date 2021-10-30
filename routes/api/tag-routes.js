cce077dedc33fea36e3d91ac60dd140cb793bfdf
Anthony Her <antpher@gmail.com>
2021-10-30 13:18:54 -0400

    updated tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -11,27 +11,51 @@ router.get('/', (req, res) => {
     include: [
       {
         model: Product,
-        attributes: ['id', 'product',]
+        attributes: ['id', 'product', 'price', 'stock', 'category_id']
       }
     ]
   })
+  .then(dbTagData => res.json(dbTagData))
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findeOne({
+    where: {id: req.params.id},
+    attributes: ['id', 'tag_name'],
+    indclude: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+  .then(dbTagData => res.json(dbTagData))
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then(dbTagData => res.json(dbTagData))
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {id: req.params.id}
+    })
+  .then(dbTagData => res.json(dbTagData))
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {id: req.params.id}
+  })
+  .then(dbTagData => res.json(dbTagData))
 });
 
 module.exports = router;
